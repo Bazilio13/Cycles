8224a538b66db80669958d51cdc4336f307f7a81
Вадим Кутищев <vki-13@yandex.ru>
2021-02-28 20:33:16 +0300

    final

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -8,17 +8,34 @@ namespace HW7
         {
             int a;
             int b;
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            while (a != b)
+            a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            if (a == 0 || b == 0)
             {
-                if (a > b)
+                if (a == b)
                 {
-                    a -= b;
+                    Console.WriteLine("оба числа равны 0, любое число является для них общим делителем");
                 }
                 else
                 {
-                    b -= a;
+                    if (b > a)
+                    {
+                        a = b;
+                    }
+                }    
+            }
+            else
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                    {
+                        a -= b;
+                    }
+                    else
+                    {
+                        b -= a;
+                    }
                 }
             }
             Console.WriteLine(a);
--- HW8/Program.cs
+++ HW8/Program.cs
@@ -6,7 +6,43 @@ namespace HW8
     {
         static void Main(string[] args)
         {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            int border1 = 1;
+            int border2 = a;
+            double check = Math.Round(Math.Pow((double)a, 1d / 3d));
+
+            if (Math.Pow(check, 3) == a)
+            {
+                while (x * x * x != a)
+                {
+                    x = (border1 + border2) / 2;
+                    if (x * x * x == a)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (x * x * x < a)
+                        {
+                            border1 = x;
+                        }
+                        else
+                        {
+                            border2 = x;
+                        }
+                    }
+                }
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("Введенное число не является кубом целого числа");
+            }
             
+
+
         }
     }
 }
